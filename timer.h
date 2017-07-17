6f702092a52d18e5b31048fa7c875101eaf96e47
baozi929 <450323716@qq.com>
2017-07-17 17:25:24 +0800

    Update timer.h

--- timer.h
+++ timer.h
@@ -10,21 +10,21 @@
 static const double clock_tics_per_sec = CLOCKS_PER_SEC;//1000000.0;
 class Timer {
    clock_t last_time;
-   clock_t curn_time;
+   clock_t cur_time;
    double  time_dur;
 
  public:
 
    void reset_clock() {
-     last_time = curn_time = clock();
+     last_time = cur_time = clock();
    }
 
    Timer() { time_dur=0.0; reset_clock();}
 
    void report_cumm_time(char* s) {              // 计算累计时间，print
 #ifdef TIMER_ON
-     curn_time = clock();
-     double tmp = (curn_time-last_time)/clock_tics_per_sec;
+     cur_time = clock();
+     double tmp = (cur_time-last_time)/clock_tics_per_sec;
      time_dur += tmp;
      printf("%s: %f secs\n",s, time_dur);
 #endif
@@ -32,20 +32,20 @@ class Timer {
 
    void report_time(char* s) {                  // 重新计时，print delta(t)
 #ifdef TIMER_ON
-     curn_time = clock();
-     double tmp = (curn_time-last_time)/clock_tics_per_sec;
+     cur_time = clock();
+     double tmp = (cur_time-last_time)/clock_tics_per_sec;
      if (tmp > 0.0) {
         printf("%s: %f secs\n",s, tmp);
       }
-     last_time = curn_time;
+     last_time = cur_time;
 #endif
     }
 
     double elapsedTime( void )                 // 重新计时，返回delta(t)
     {
-      curn_time = clock();
-      double tmp = (curn_time - last_time) / clock_tics_per_sec;
-      last_time = curn_time;
+      cur_time = clock();
+      double tmp = (cur_time - last_time) / clock_tics_per_sec;
+      last_time = cur_time;
       return tmp;
     }
  };
